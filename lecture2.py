b919cbd81ff555fa29195db5469c0fc61668096d
Sadikul Haque Sadi <bsse1003@iit.du.ac.bd>
2026-04-21 23:47:11 +0600

    lecture 2 contents added
    lecture 2 includes the use of operators, formatted string output and user input

---
+++ lecture2.py
@@ -0,0 +1,56 @@
+a = 10
+b = 20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#examples of a +=, a -=, a *=, a /=, a //=, a %=, a **=
+a += 5
+print(a)
+a -= 3
+print(a)
+a *= 2
+print(a)
+a /= 4
+print(a)
+a //= 2
+print(a)
+a %= 3
+print(a)
+
+#user input
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+#foramtted string
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f'Hello {name}, you are {age} years old.')
+
+#print function using sep and end
+print("Hello", "World", sep="-", end="!\n")
+
+#Excerse 2: check if a number is even or odd
+number = int(input("Enter a number: "))
+print(f'{number} is even? {number % 2 == 0}')
+
+#try yourself: given total number of days, calculate the total years, months, weeks, and remaining days. Assume 1 year = 365 days, 1 month = 30 days, and 1 week = 7 days.
+total_days = int(input("Enter the total number of days: "))
+years = total_days // 365
+remaining_days = total_days % 365
+months = None #try yourself
+weeks = None #try yourself
+remaining_days = remaining_days % 7
+print(f"Years: {years}, Months: {months}, Weeks: {weeks}, Days: {remaining_days}")
